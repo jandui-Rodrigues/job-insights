fff976a4f9dae96a3386be64ce8e5a337ad73345
jandui <janduineto18@gmail.com>
2023-11-23 15:03:32 -0300

    feat: add filter by multiple criteria in Process jobs

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -6,8 +6,8 @@ class ProcessJobs:
     def __init__(self) -> None:
         self.jobs_list = list()
 
-    def read(self, path='data/jobs.csv') -> List[Dict]:
-        file = open(path, 'r')
+    def read(self, path="data/jobs.csv") -> List[Dict]:
+        file = open(path, "r")
         content = csv.DictReader(file, delimiter=",", quotechar='"')
         header, *data = content
         self.jobs_list = [header, *data]
@@ -16,16 +16,17 @@ class ProcessJobs:
         unique_type = []
 
         for job in self.jobs_list:
-            type = job['job_type']
+            type = job["job_type"]
             if type not in unique_type:
                 unique_type.append(type)
         return unique_type
 
-    def filter_by_multiple_criteria(self) -> List[dict]:
-        pass
+    def filter_by_multiple_criteria(self, job_list, filter) -> List[dict]:
+        if not isinstance(filter, dict):
+            raise TypeError
+        industry = filter["industry"]
+        job_type = filter["job_type"]
 
-
-ppp = ProcessJobs()
-ppp.read()
-print(ppp.jobs_list[0])
-print(ppp.get_unique_job_types())
+        return [job for job in job_list
+                if job['job_type'] == job_type
+                and job["industry"] == industry]
