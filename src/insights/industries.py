df629e7851ea33f976f96ec63f72e4c8af0d7477
jandui <janduineto18@gmail.com>
2023-11-23 15:18:47 -0300

    feat: add get unique industry

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -7,4 +7,17 @@ class ProcessIndustries(ProcessJobs):
         super().__init__()
 
     def get_unique_industries(self) -> List[str]:
-        pass
+        unique_industry = []
+
+        for job in self.jobs_list:
+            type = job["industry"]
+            if type not in unique_industry and type != '':
+                unique_industry.append(type)
+
+        print(unique_industry)
+        return unique_industry
+
+
+p = ProcessIndustries()
+p.read()
+print('ok')
