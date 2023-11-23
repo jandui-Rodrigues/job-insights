b8d87cc81b25ecaeaf0b9a81635cd3f70d25e53d
jandui <janduineto18@gmail.com>
2023-11-23 13:17:49 -0300

    test: Create Test for Counter words

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    raise
+    assert count_ocurrences('data/jobs.csv', 'Python') == 1639
+    assert count_ocurrences('data/jobs.csv', 'python') == 1639
+    assert count_ocurrences('data/jobs.csv', 'JavaScript') == 122
+    assert count_ocurrences('data/jobs.csv', 'javaScript') == 122
