911abead622a9619a1e9c8bc104129b6b482e4ee
jandui <janduineto18@gmail.com>
2023-11-23 21:59:41 -0300

    feat:  add fuction maches Salary range

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -8,18 +8,39 @@ class ProcessSalaries(ProcessJobs):
 
     def get_max_salary(self) -> int:
         # min_salary
-        max_salary = [int(job['max_salary']) for job in self.jobs_list
-                      if job['max_salary'].isdigit()]
+        max_salary = [
+            int(job["max_salary"])
+            for job in self.jobs_list
+            if job["max_salary"].isdigit()
+        ]
         return max(max_salary)
 
     def get_min_salary(self) -> int:
         # min_salary
-        min_salary = [int(job['min_salary']) for job in self.jobs_list
-                      if job['min_salary'].isdigit()]
+        min_salary = [
+            int(job["min_salary"])
+            for job in self.jobs_list
+            if job["min_salary"].isdigit()
+        ]
         return min(min_salary)
 
+    def validate_job(self, min_salary, max_salary):
+        if min_salary == "" or max_salary == "":
+            raise ValueError()
+        if max_salary < min_salary:
+            raise ValueError()
+
     def matches_salary_range(self, job: Dict, salary: Union[int, str]) -> bool:
-        pass
+        try:
+            min_salary = int(job["min_salary"])
+            max_salary = int(job["max_salary"])
+            self.validate_job(min_salary, max_salary)
+            if int(salary) <= max_salary and int(salary) >= min_salary:
+                return True
+            else:
+                return False
+        except (TypeError, ValueError, KeyError):
+            raise ValueError
 
     def filter_by_salary_range(
         self, jobs: List[dict], salary: Union[str, int]
